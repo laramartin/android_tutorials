5260a6e5913adee43813446e7ca43d355e66de0d
Lara Martin <lara.martin.carretero@gmail.com>
2017-06-05 20:12:49 +0200

    implemented collapsing toolbar layout but NestedScrollView isn't scrolling until the end

---
+++ scrolling/ScrollingSamples/app/src/main/java/eu/laramartin/scrollingsamples/MainActivity.java
@@ -0,0 +1,21 @@
+package eu.laramartin.scrollingsamples;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CollapsingToolbarLayout toolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
+        toolbarLayout.setTitle(getString(R.string.app_name));
+        toolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
+        toolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this, R.color.colorPrimary));
+    }
+}
